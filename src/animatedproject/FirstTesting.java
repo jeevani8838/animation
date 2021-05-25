ba090dedc0796cef43604fbfeddfa1486f2232d0
jivan <jivan@LAPTOP-LT6IS0EJ>
2021-05-25 16:31:03 -0400

    hi this is jeevani

---
+++ src/animatedproject/FirstTesting.java
@@ -0,0 +1,16 @@
+package animatedproject;
+
+public class FirstTesting {
+
+	public static void main(String[] args) {
+		// This is first testing of animation project through 
+		// Jenkins server
+		
+		int x = 50;
+		int y = 30;
+		System.out.println(x*y);
+		System.out.println("This is Jeevani DevOps Engineer");
+
+	}
+
+}
